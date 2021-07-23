fb8ae308c0f7921a43e9f9ca5f51c87225fafc01
Liviu Maria <lmaria@grubhub.com>
2021-07-23 18:00:08 +0300

    sol

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Run, you fool! HAHAH");
+    let n = 1000;
+    let mut accumulator = 0;
+    for i in 3..n {
+        if (i % 3 == 0) || (i % 5 == 0) {
+            accumulator = accumulator + i;
+        }
+    }
+
+    println!("{}", accumulator);
 }
